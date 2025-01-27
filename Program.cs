24c7d86165714e6cc0c1f0d83d6652989c4e4193
Christofer Carlsson <113172285+ChristoferCarlsson@users.noreply.github.com>
2025-01-27 09:38:35 +0100

    Created project and db

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chatroom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Show all of our data from db
+
+            // Entity Framework Core
+            using (var db = new UserDbContext())
+            {
+                //Ensure that database is created
+                db.Database.EnsureCreated();
+
+                //Query and display users in database
+                var users = db.Users.ToList();
+                Console.WriteLine("Users in the database");
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.UserId}: {user.UserName}: {user.UserPassword}: {user.Email}:");
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatroom
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(local)\\SQLEXPRESS;Database=ChatRoomDB;Trusted_Connection=true;TrustServerCertificate=True"); //Connection string to db
+        }
+    }
+}
