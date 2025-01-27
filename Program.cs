4c8b35589eb3334a19f6338da2d5cd12f0e35f18
NahomHadgebes <Hadgebesnahom@gmail.com>
2025-01-27 12:59:59 +0100

    Created a Reset Password function

--- Program.cs
+++ Program.cs
@@ -9,22 +9,32 @@ namespace Chatroom
             // Show all of our data from db
 
             // Entity Framework Core
+            //using (var db = new UserDbContext())
+            //{
+            //    //Ensure that database is created
+            //    db.Database.EnsureCreated();
+
+            //    //Query and display users in database
+            //    var users = db.Users.ToList();
+            //    Console.WriteLine("Users in the database");
+            //    foreach (var user in users)
+            //    {
+            //        Console.WriteLine($"{user.UserId}: {user.UserName}: {user.UserPassword}: {user.Email}:");
+            //    }
+
             using (var db = new UserDbContext())
             {
-                //Ensure that database is created
                 db.Database.EnsureCreated();
 
-                //Query and display users in database
-                var users = db.Users.ToList();
-                Console.WriteLine("Users in the database");
-                foreach (var user in users)
-                {
-                    Console.WriteLine($"{user.UserId}: {user.UserName}: {user.UserPassword}: {user.Email}:");
-                }
+                var forgotPassword = new ForgotPassword(db);
+
+
+
+                forgotPassword.ResetPassword();
             }
 
         }
+       
     }
-
 
 }
